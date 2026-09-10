9eee9ca909dac47768ff6b3f1a6a3d671fb5f135
Youssef-Wheed <wayoussef138@gmail.com>
2026-09-10 22:08:37 +0300

    Add homepage.dart file

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:furniture_shop_app/Widgets/CustomText.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../Widgets/CustomSearch.dart';
+
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
@@ -9,21 +11,27 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-     appBar: AppBar(
-       automaticallyImplyLeading: false,
-       backgroundColor: Colors.white,
-       title: Padding(
-         padding: const EdgeInsets.symmetric(horizontal: 8.0),
-         child: Row(
-           children: [
-             Customtext(text: 'Discover The Best\nFurniture.',),
-             Spacer(),
-             Image.asset('assets/HomePage/profile.png',width: 54,height: 54,),
-           ],
-         ),
-       ),
-     ),
-
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: [
+              Customtext(text: 'Discover The Best\nFurniture.'),
+              Spacer(),
+              Image.asset('assets/HomePage/profile.png', width: 54, height: 54),
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          //search
+          Customsearch(),
+        ],
+      ),
     );
   }
 }
